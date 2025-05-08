8652d0c4955b34ae7199f33a7c8b52f721c67793
Ambrosiavino <enrico.patrizio@ambrosiavino.com>
2025-05-08 23:31:10 +0200

    Update DionisoIntro.js

--- components/DionisoIntro.js
+++ components/DionisoIntro.js
@@ -1,10 +1,36 @@
 export default function DionisoIntro({ onAvviaGioco }) {
   return (
-    <div style={{ textAlign: 'center', padding: 20 }}>
-      <h1>Benvenuto esploratore!</h1>
-      <p>Sono Dioniso, ti guiderò alla scoperta di Ambrosia.</p>
-      <button onClick={onAvviaGioco}>Inizia la degustazione</button>
-      <button onClick={() => alert('Profilo del vignaiolo in arrivo!')}>Scopri il vignaiolo</button>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '2rem',
+      background: '#fff5e6',
+      borderRadius: '1rem',
+      fontFamily: 'Georgia, serif',
+    }}>
+      <img
+        src="/dioniso.png"
+        alt="Dioniso"
+        style={{
+          width: '180px',
+          height: 'auto',
+          marginRight: '2rem',
+          borderRadius: '1rem',
+        }}
+      />
+      <div>
+        <h1 style={{ marginBottom: '0.5rem' }}>Benvenuto esploratore!</h1>
+        <p style={{ marginBottom: '1rem' }}>
+          Sono Dioniso, ti guiderò alla scoperta di Ambrosia.
+        </p>
+        <button onClick={onAvviaGioco} style={{ marginRight: '1rem' }}>
+          Inizia la degustazione
+        </button>
+        <button onClick={() => alert('Profilo del vignaiolo in arrivo!')}>
+          Scopri il vignaiolo
+        </button>
+      </div>
     </div>
   );
 }
